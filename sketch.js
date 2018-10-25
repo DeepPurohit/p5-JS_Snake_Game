f9f2d68736b4f5abd46c070d6e1192a48d5ceb4e
Deep Purohit <DeepPurohit@users.noreply.github.com>
2018-10-25 21:14:42 +0530

    Files uploaded

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var s;
+var scl = 20;
+
+var food;
+
+function setup() {
+  var cnv = createCanvas(700, 600);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  s = new Snake();
+  frameRate(10);
+  pickLocation();
+
+}
+
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function mousePressed() {
+  s.total++;
+}
+
+function draw() {
+  //background(51);
+  background(10, 155, 240);
+
+  if (s.eat(food)) {
+    pickLocation();
+  }
+  s.death();
+  s.update();
+  s.show();
+
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
+
+
+
+
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
+}
